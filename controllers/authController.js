13172d31393cf07063feb0f2e5d64dd5d021f02f
adeeshperera <user.adeeshperera@hotmail.com>
2025-10-30 20:55:33 +0530

    apex-backend

---
+++ controllers/authController.js
@@ -0,0 +1,66 @@
+const User = require('../models/User');
+const { generateToken } = require('../config/jwt');
+
+const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      passwordHash: password,
+    });
+
+    const token = generateToken(user._id);
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (user && (await user.matchPassword(password))) {
+      const token = generateToken(user._id);
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token,
+      });
+    } else {
+      res.status(401).json({ message: 'Invalid email or password' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+};
+
+const getProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).select('-passwordHash');
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+};
+
+module.exports = {
+  register,
+  login,
+  getProfile,
+};
---
+++ controllers/serviceController.js
@@ -0,0 +1,28 @@
+const Service = require('../models/Service');
+
+const getServices = async (req, res) => {
+  try {
+    const services = await Service.find({});
+    res.json(services);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+};
+
+const getServiceById = async (req, res) => {
+  try {
+    const service = await Service.findById(req.params.id);
+    if (service) {
+      res.json(service);
+    } else {
+      res.status(404).json({ message: 'Service not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+};
+
+module.exports = {
+  getServices,
+  getServiceById,
+};
